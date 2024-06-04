6ef0b580178bb62b00bb12af3f01a4075a1c6b9a
Spare <83568700+SpareCeo@users.noreply.github.com>
2024-06-04 23:21:59 +0300

    Update install_spare.sh

--- install_spare.sh
+++ install_spare.sh
@@ -4,31 +4,29 @@ ENDCOLOR="\e[0m"
 
 clear
 
+# Remove existing files
 if [ -f "spare_install.sh" ]; then
     rm spare_install.sh
-    sleep 1
 fi
 
 if [ -f "tek" ]; then
     echo -e "${RED}Deleting old items.dat...${ENDCOLOR}"
     rm tek
-    sleep 1
     echo -e "${GREEN}Updating items.dat...${ENDCOLOR}"
 fi
 
-wget -q https://raw.githubusercontent.com/SpareCeo/spare-auth/main/items.dat
+# Download items.dat
+wget -qN https://raw.githubusercontent.com/SpareCeo/spare-auth/main/items.dat
 echo -e "${GREEN}items.dat Downloaded${ENDCOLOR}"
 
+# Install Spare Proxy
 echo -e "${GREEN}Installing Spare Proxy...${ENDCOLOR}"
-sleep 1
 if [ -f "spare" ]; then
     echo -e "${RED}Deleting old spare proxy...${ENDCOLOR}"
     rm spare
-    sleep 1
     echo -e "${GREEN}Updating spare proxy...${ENDCOLOR}"
 fi
-wget -q https://raw.githubusercontent.com/SpareCeo/spare-auth/main/spare
-sleep 1
+wget -qN https://raw.githubusercontent.com/SpareCeo/spare-auth/main/spare
 echo -e "${GREEN}Spare Proxy Installed${ENDCOLOR}"
 echo -e "${GREEN}Execute spare proxy with this command: ./spare${ENDCOLOR}"
 chmod +x spare
