e2e07972dbe86d03bbe188d5de43b72ccc1b0aa4
Spare <83568700+SpareCeo@users.noreply.github.com>
2024-06-04 22:49:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,15 +4,15 @@ ENDCOLOR="\e[0m"
 
 clear
 
-if [ -f "items" ]; then
-    echo -e "${RED}Deleting Old Items.dat...${ENDCOLOR}"
-    rm items
+if [ -f "items.dat" ]; then
+    echo -e "${RED}Deleting Old items.dat...${ENDCOLOR}"
+    rm items.dat
     sleep 1
-    echo -e "${GREEN}Updating Items.dat...${ENDCOLOR}"
+    echo -e "${GREEN}Updating items.dat...${ENDCOLOR}"
 fi
 
-wget -q https://github.com/SpareCeo/spare-auth/blob/main/items.dat
-echo -e "${GREEN}Items.dat Downloaded${ENDCOLOR}"
+wget -q https://github.com/SpareCeo/spare-auth/raw/main/items.dat
+echo -e "${GREEN}items.dat Downloaded${ENDCOLOR}"
 
 if [ -f "spare" ]; then
     echo -e "${RED}Deleting Old Spare Proxy...${ENDCOLOR}"
@@ -20,7 +20,8 @@ if [ -f "spare" ]; then
     sleep 1
     echo -e "${GREEN}Updating Spare Proxy...${ENDCOLOR}"
 fi
-wget -q https://github.com/SpareCeo/spare-auth/blob/main/spare
+
+wget -q https://github.com/SpareCeo/spare-auth/raw/main/spare
 sleep 1
 echo -e "${GREEN}Spare Proxy Installed${ENDCOLOR}"
 echo -e "${GREEN}Execute Spare Proxy with this command: ./spare${ENDCOLOR}"
